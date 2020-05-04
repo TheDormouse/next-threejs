84163ca5613a1855c1d80570a51fd43fcd68c39a
Ben Martinez-Cain <bmc@bens-air.lan>
2020-05-04 02:51:41 -0400

    add/remove objects

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,41 @@ export default (props) => {
     const [height, setHeight] = useState(null)
     const [width, setWidth] = useState(null)
     const [objects, setObjects] = useState([])
+
+    function getRandomInt(allowNeg ,max) {
+        let num =  Math.floor(Math.random() * Math.floor(max)) + 1;
+        if(allowNeg){
+            num *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+        }
+        return num
+      }
+    function createObject() {
+        let type;
+        let random = getRandomInt(false, 11);
+        if(random <= 5){
+            type = 'Sphere'
+        } else {
+            type = 'Box'
+        }
+        let obj = {
+            type: type, 
+            coords: [getRandomInt(true, 5), getRandomInt(true, 5), getRandomInt(true, 5)]
+        }
+        return obj
+    }
+    function init(){
+        let generateObjects = []
+        for(let i =0; i< 100; i++) {
+            generateObjects.push(createObject())
+        }
+        return generateObjects;
+    }
+    function addObject() {
+        setObjects([...objects, createObject()])
+    }
+    function removeObject() {
+        setObjects(objects.splice(0, objects.length - 1))
+    }
     useEffect(() => {
         function handleResize(){
             setHeight(window.innerHeight);
@@ -16,47 +51,45 @@ export default (props) => {
         setHeight(window.innerHeight)
         setWidth(window.innerWidth)
         window.addEventListener('resize', handleResize)
-        function init(){
-            let generateObjects = []
-            for(let i =0; i< 100; i++) {
-                function getRandomInt(allowNeg ,max) {
-                    let num =  Math.floor(Math.random() * Math.floor(max)) + 1;
-                    if(allowNeg){
-                        num *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
-                    }
-                    return num
-                  }
-                let type;
-                let random = getRandomInt(false, 11);
-                if(random <= 5){
-                    type = 'Sphere'
-                } else {
-                    type = 'Box'
-                }
-                generateObjects.push({type: type, coords: [getRandomInt(true, 5), getRandomInt(true, 5), getRandomInt(true, 5)]})
-            }
-            return generateObjects;
-        }
+        
     
         setObjects(init());
     }, []
     )
-
-    return( <Canvas style={{height:height,width:width}}>
+    return( 
+    
+    <div>
+        <style jsx>{`
+        .overlay {
+            background-color: blue;
+            width: 100%;
+            position: absolute;
+            bottom: 0px;
+            z-index: 2;
+        }
+        `}</style>
         <style jsx global> {`
         body {
           margin: 0px;
         }
       `}</style>
+        <div className='overlay'>
+            <button onClick={() => addObject()}>Add Object</button>
+            <button onClick={() => removeObject()}>Remove Object</button>
+            <p>Number of objects: {objects.length}</p>
+            </div>
+            <Canvas style={{height:height,width:width}}>
+        
       
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        {objects.map((object, i) => {
+        {objects && objects.map((object, i) => {
 
             return React.createElement(Models[object.type], {
                 position: object.coords,
                 key: i
             })
         })}
-      </Canvas>)
+      </Canvas>
+      </div>)
 }
